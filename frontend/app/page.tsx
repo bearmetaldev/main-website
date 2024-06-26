64586e6f87b9ec9cf58944a12329a08b6c66fc10
Nour Ghallale <165909521+nourgm10@users.noreply.github.com>
2024-06-26 16:44:16 +0200

    modified the engineering section: changed the background, added the animation and a button

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,6 @@
 import Hero from "./components/hero/hero";
 import Web3 from "./components/web3/web3";
-import Engineering from "./components/engineering";
+import Engineering from "./components/engineering/engineering";
 import Trainings from "./components/trainings";
 import Contact from "./components/contact";
 import Navbar from "./components/navbar/navbar";

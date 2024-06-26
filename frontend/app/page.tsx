31273cb3754d09967c9441a15bc0c964efc64857
Nour Ghallale <165909521+nourgm10@users.noreply.github.com>
2024-06-26 18:01:17 +0200

    updated trainings section with animated cards and background

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "./components/hero/hero";
 import Web3 from "./components/web3/web3";
 import Engineering from "./components/engineering/engineering";
-import Trainings from "./components/trainings";
+import Trainings from "./components/trainings/trainings";
 import Contact from "./components/contact";
 import Navbar from "./components/navbar/navbar";
 

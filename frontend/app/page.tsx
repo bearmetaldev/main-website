e9dd9e9bc879a6ae56a1730757b9976ac8a00014
Nour Ghallale <165909521+nourgm10@users.noreply.github.com>
2024-06-26 12:11:14 +0200

    updated hero: built navbar with hover animation, changed background image and added gradient, added text with gradient effect and created button with hover animation

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,4 @@
-import Hero from "./components/hero";
+import Hero from "./components/hero/hero";
 import Web3 from "./components/web3";
 import Engineering from "./components/engineering";
 import Trainings from "./components/trainings";

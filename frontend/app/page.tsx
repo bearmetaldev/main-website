c5456ae1d60f9832ed861363cbab090a3a667fe2
Nour Ghallale <165909521+nourgm10@users.noreply.github.com>
2024-06-27 12:16:19 +0200

    updated contact section: created grid background, created glassmorphic contact card with hover animation

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "./components/hero/hero";
 import Web3 from "./components/web3/web3";
 import Engineering from "./components/engineering/engineering";
 import Trainings from "./components/trainings/trainings";
-import Contact from "./components/contact";
+import Contact from "./components/contact/contact";
 import Navbar from "./components/navbar/navbar";
 
 export default function Page() {

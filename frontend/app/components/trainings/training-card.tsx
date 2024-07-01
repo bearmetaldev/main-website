c1129d01fbd5ca4e485f0d4209b8d4b8995e522f
Nour Ghallale <165909521+nourgm10@users.noreply.github.com>
2024-07-01 15:38:11 +0200

    made trainings section responsive

--- frontend/app/components/trainings/training-card.tsx
+++ frontend/app/components/trainings/training-card.tsx
@@ -14,12 +14,18 @@ const TrainingCard: React.FC<TrainingCardProps> = ({
   content,
 }) => {
   return (
-    <div className="group relative w-full max-w-sm overflow-hidden rounded-lg bg-slate-600/30 p-0.5 transition-all duration-500 hover:scale-[1.01] hover:bg-slate-800/50 cursor-pointer">
-      <div className="relative z-10 flex flex-col items-center justify-center overflow-hidden rounded-[7px] bg-slate-800/10 px-8 py-20 transition-colors duration-500 group-hover:bg-slate-950/30">
-        <div className="relative z-10 mb-8 mt-2 h-20">
-          <Image src={icon} alt={title} width={140} height={140} />
+    <div className="group relative w-full max-w-80 md:max-w-xl lg:max-w-sm overflow-hidden rounded-lg bg-slate-600/30 p-0.5 transition-all duration-500 lg:hover:scale-[1.01] lg:hover:bg-slate-800/50 cursor-pointer">
+      <div className="relative z-10 flex flex-col items-center justify-center overflow-hidden rounded-[7px] bg-slate-800/10 px-6 py-5 md:py-16 lg:py-20 transition-colors duration-500 lg:group-hover:bg-slate-950/30">
+        <div className="relative z-10 md:mb-6 lg:mb-8 md:mt-2 h-16 lg:h-20">
+          <Image
+            src={icon}
+            alt={title}
+            width={140}
+            height={140}
+            className="w-[70%] h-[70%] md:w-[100%] md:h-[100%] object-contain"
+          />
         </div>
-        <p className="relative z-10 text-center text-base leading-relaxed text-gray-200">
+        <p className="relative z-10 text-center text-base leading-relaxed text-gray-200 trainings-description">
           {content.split(" ").slice(0, 50).join(" ") + "..."}
         </p>
       </div>
@@ -32,7 +38,7 @@ const TrainingCard: React.FC<TrainingCardProps> = ({
           duration: 15,
           ease: "linear",
         }}
-        className="absolute inset-0 z-0 bg-gradient-to-br from-indigo-200 via-indigo-200/0 to-indigo-200 opacity-40 transition-opacity duration-500 group-hover:opacity-50"
+        className="absolute inset-0 z-0 bg-gradient-to-br from-indigo-200 via-indigo-200/0 to-indigo-200 opacity-40 transition-opacity duration-500 lg:group-hover:opacity-50"
       />
     </div>
   );
--- frontend/app/components/trainings/trainings-content.tsx
+++ frontend/app/components/trainings/trainings-content.tsx
@@ -3,9 +3,9 @@ import TrainingCard from "./training-card";
 
 export default function TrainingsContent() {
   return (
-    <div className="h-full flex flex-col items-center justify-center px-4 py-12 relative z-10">
-      <h1 className=" text-white mb-20">Trainings</h1>
-      <div className="flex flex-wrap justify-center gap-8 max-w-5xl">
+    <div className="h-full flex flex-col items-center justify-center px-4 py-8 md:py-10 lg:py-12 relative z-10 mt-2 md:mt-0">
+      <h1 className="text-white mb-4 md:mb-16 lg:mb-20">Trainings</h1>
+      <div className="flex flex-col lg:flex-row items-center justify-center gap-8 md:gap-12 max-w-5xl w-full">
         <TrainingCard
           icon="/rust-logo.png"
           title="Rust"

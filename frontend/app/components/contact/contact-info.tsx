374222edfcbdafcdb89300a12f6e827aa46bdef8
Nour Ghallale <165909521+nourgm10@users.noreply.github.com>
2024-07-05 15:07:40 +0200

    Fix: updated contact info (#12)

--- frontend/app/components/contact/contact-info.tsx
+++ frontend/app/components/contact/contact-info.tsx
@@ -5,23 +5,7 @@ export default function ContactInfo() {
         <p className="text-left mb-4 md:mx-12 text-lg">
           <strong>Email</strong>
           <br />
-          contact@example.com
-        </p>
-        <p className="text-left md:mx-12 text-lg">
-          <strong>Phone</strong>
-          <br />
-          +1 234 567 8900
-        </p>
-      </div>
-      <div className="w-1/2 lg:w-full">
-        <p className="text-left md:mx-12 text-lg">
-          <strong>Address</strong>
-          <br />
-          123 Example Street
-          <br />
-          City, State 12345
-          <br />
-          Country
+          contact@bearmetaldev.com
         </p>
       </div>
     </div>
